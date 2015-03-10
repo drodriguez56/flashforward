06da07ec9a4bf3819fa69d3a8613a5ad6efce2bb
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-03-10 15:28:44 -0400

    Add jquery to change image on phone when scroll

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   $(window).scroll(function(){
         if ($(this).scrollTop() >= $(".section-3").offset().top) {
-           $(".inner-phone").css("background-color","blue");
+          $(".cell-phone-img").attr("src","img/flashF.jpg");
+          $(".phone").addClass("fixed")
         }else if($(this).scrollTop() >= $(".section-2").offset().top) {
-          $(".inner-phone").css("background-color","yellow");
+          $(".cell-phone-img").attr("src","img/fun.jpg");
+          $(".phone").addClass("fixed")
         } else {
           $(".inner-phone").css("background-color","green");
+          $(".phone").removeClass("fixed")
         }
     });
 });
