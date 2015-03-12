b782ab60aa3b549228feee46fff393087d311327
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-03-12 09:23:24 -0400

    changes on img format

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $(window).scroll(function(){
         if ($(this).scrollTop() >= $(".section-3").offset().top) {
-          $(".cell-phone-img").attr("src","img/flashF.jpg");
+          $(".cell-phone-img").attr("src","img/flashF.png");
           $(".phone").addClass("fixed")
         }else if($(this).scrollTop() >= $(".section-2").offset().top) {
-          $(".cell-phone-img").attr("src","img/fun.jpg");
+          $(".cell-phone-img").attr("src","img/fun.png");
           $(".phone").addClass("fixed")
         } else {
           $(".inner-phone").css("background-color","green");
